0d446de94a3add981d2387f027de95c43e810416
swajal14 <swajalkumarkachhi@gmail.com>
2026-06-30 22:46:21 +0530

    feat: Exceptions added

--- src/com/stschool/ecommerce/exception/CustomerExistsException.java
+++ src/com/stschool/ecommerce/exception/CustomerExistsException.java
@@ -3,5 +3,6 @@ package com.stschool.ecommerce.exception;
 public class CustomerExistsException extends Exception{
     public CustomerExistsException(String message) {
         super(message);
+        
     }
 }
--- src/com/stschool/ecommerce/exception/InvalidCredentialsException.java
+++ src/com/stschool/ecommerce/exception/InvalidCredentialsException.java
@@ -3,5 +3,6 @@ package com.stschool.ecommerce.exception;
 public class InvalidCredentialsException extends Exception{
     public InvalidCredentialsException(String message) {
         super(message);
+        
     }
 }
